49c3f71b2c131bc3e793e67bec55b6c03114b3a8
shigongpeng <503484839@qq.com>
2019-09-22 21:30:54 +0800

    [*]socket

---
+++ src/main/java/com/chrishi/springmvccode/serversocket/Client.java
@@ -0,0 +1,34 @@
+package com.chrishi.springmvccode.serversocket;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.nio.Buffer;
+
+public class Client {
+
+    public static void main(String[] args) {
+        String msg = "Client Data";
+        try {
+            //创建一个Socket，根本机的8080端口连接
+            Socket socket = new Socket("127.0.0.1",8080);
+            //使用Socket创建PrintWriter和BufferedReader进行读写数据
+            PrintWriter pw = new PrintWriter(socket.getOutputStream());
+            BufferedReader is = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            //发送数据
+            pw.println(msg);
+            pw.flush();
+            //接收数据
+            String line = is.readLine();
+            System.out.println("received from server:"+line);
+            //关闭资源
+            pw.close();
+            is.close();
+            socket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
